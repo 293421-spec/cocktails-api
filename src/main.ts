fb1c934f4128c17d8b27fdd960620964ce9147af
293421-spec <293421@student.pwr.edu.pl>
2026-03-06 11:43:56 +0100

    feat:enable CORS

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,8 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors();
+
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
 
   const config = new DocumentBuilder()
@@ -19,7 +21,7 @@ async function bootstrap() {
 
   const port = process.env.PORT || 3000;
   await app.listen(port);
-  console.log(` API działa na porcie ${port}`);
-  console.log(` Swagger: http://localhost:${port}/api`);
+  console.log(`API działa na porcie ${port}`);
+  console.log(`Swagger: http://localhost:${port}/api`);
 }
 bootstrap();
